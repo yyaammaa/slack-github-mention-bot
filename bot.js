ce8e16d9151b799093dfb1ff95fe0bf0bbe9c03c
yamabit <yama@supership.jp>
2016-04-08 18:31:32 +0900

    log

--- bot.js
+++ bot.js
@@ -11,24 +11,42 @@ bot.started((payload) => {
 });
 
 bot.message((msg) => {
+  console.log('msg received');
   console.log(msg);
 
-  if (!msg.user) return;
+  // handle bot message
+  if (msg.subtype && msg.subtype === 'bot_message') {
+    let attachments = msg.attachments;
+    if (!attachments) {
+      console.log('no attachments');
+      return;
+    }
+    _.each(attachments, at => {
+      const text = at.text || '(no text)';
+      const fallback = at.fallback || '(no fallback)';
+      console.log('text = ' + text + ', fallback = ' + fallback);
+    });
 
-  if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) return;
+    return;
+  }
 
-  slack.chat.postMessage({
-    token: config('SLACK_TOKEN'),
-    //icon_emoji: config('ICON_EMOJI'),
-    channel: msg.channel,
-    username: 'yamaboot',
-    text: `beep boop: I hear you loud and clear!"`
-  }, (err, data) => {
-    if (err) throw err;
-
-    let txt = _.truncate(data.message.text);
-    console.log('message posted: ' + txt);
-  })
+  //if (!msg.user) return;
+  //
+  //if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) return;
+  //
+  //slack.chat.postMessage({
+  //  token: config('SLACK_TOKEN'),
+  //  //icon_emoji: config('ICON_EMOJI'),
+  //  channel: msg.channel,
+  //  username: 'yamaboot',
+  //  text: `beep boop: I hear you loud and clear!"`
+  //}, (err, data) => {
+  //  if (err) throw err;
+  //
+  //  let txt = _.truncate(data.message.text);
+  //  console.log('message posted: ' + txt);
+  //})
+  //
 });
 
 module.exports = bot;
