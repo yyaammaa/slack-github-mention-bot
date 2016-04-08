3a3da9bc9dc989de5760cf124361c2f7fb209a10
yamabit <yama@supership.jp>
2016-04-08 18:43:27 +0900

    reply

--- bot.js
+++ bot.js
@@ -14,29 +14,52 @@ bot.message((msg) => {
   console.log('msg received');
   console.log(msg);
 
-  // handle bot message
-  if (msg.subtype && msg.subtype === 'bot_message') {
-    let attachments = msg.attachments;
-    if (!attachments) {
-      console.log('no attachments');
-      return;
-    }
-
-    let hasMention = false;
-    let isGitHub = false;
-
-    _.each(attachments, at => {
-      const text = at.text || '(no text)';
-      const fallback = at.fallback || '(no fallback)';
-      //console.log('text = ' + text + ', fallback = ' + fallback);
-      hasMention = text.includes('@yamabit');
-      isGitHub = fallback.includes('github.com');
-      console.log('hasMention = ' + hasMention + ', isGitHub = ' + isGitHub);
-      if (hasMention && isGitHub) return false;
-    });
-
+  if (!msg.subtype || msg.subtype !== 'bot_message') {
+    console.log('not a bot message');
     return;
   }
+
+  // handle bot message
+  let attachments = msg.attachments;
+  if (!attachments) {
+    console.log('no attachments');
+    return;
+  }
+
+  let hasMention = false;
+  let isGitHub = false;
+
+  _.each(attachments, at => {
+    const text = at.text || '(no text)';
+    const fallback = at.fallback || '(no fallback)';
+    //console.log('text = ' + text + ', fallback = ' + fallback);
+    hasMention = text.includes('@yamabit');
+    isGitHub = fallback.includes('github.com');
+    console.log('hasMention = ' + hasMention + ', isGitHub = ' + isGitHub);
+    if (hasMention && isGitHub) return false;
+  });
+
+  if (!hasMention || !isGitHub) {
+    console.log('not a mention from github');
+    return;
+  }
+
+  slack.chat.postMessage({
+    token: config('SLACK_TOKEN'),
+    //icon_emoji: config('ICON_EMOJI'),
+    channel: msg.channel,
+    username: 'mentionbot',
+    text: '@yama you mentioned on GitHub',
+    link_names: 1
+  }, (err, data) => {
+    if (err) {
+      console.log('postMessage failed: ' + err);
+      throw err;
+    }
+
+    let txt = _.truncate(data.message.text);
+    console.log('message posted: ' + txt);
+  });
 
   //if (!msg.user) return;
   //
